d7259c867223172006c936e70661f276fb139921
AbhishekYadav001184 <0111ay7392@gmail.com>
2026-02-03 11:43:53 +0530

    Time: 2101 ms (32.19%) | Memory: 42.4 MB (6.46%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+      boolean per=true;
+      int sum=0;
+      for(int i=1;i<num;i++){
+        if(num%i==0){
+            sum=sum+i;
+            
+        }
+        
+      }
+      if(sum!=num){
+        per=false;
+      }
+      else{
+        per=true;
+      }
+      return per;
+    }
+}
